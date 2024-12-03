d0b13f865e41cb6a5910aa4bdb48260b4bf5f17e
ihamuma <markus.ihamuotila@gmail.com>
2024-12-03 22:12:39 +0200

    loopy loops

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -2,21 +2,41 @@ fn main() {
     let number = 6;
 
     if number % 4 == 0 {
-        println!("number is divisible by 4");
+        println!("Number is divisible by 4");
     } else if number % 3 == 0 {
-        println!("number is divisible by 3");
+        println!("Number is divisible by 3");
     } else if number % 2 == 0 {
-        println!("number is divisible by 2");
+        println!("Number is divisible by 2");
     } else {
-        println!("number is not divisible by 4, 3, or 2");
+        println!("Number is not divisible by 4, 3, or 2");
     }
 
     let_statement(false);
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 8 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
 }
 
 fn let_statement(condition: bool) {
     let number = if condition { 5 } else { 6 };
-//  let number = if condition { 5 } else { "six" }; This won't compile bc Rust needs to know the type of number ahead of time
+//  let Number = if condition { 5 } else { "six" }; This won't compile bc Rust needs to know the type of Number ahead of time
 
-    println!("The value of the number is {number}")
+    println!("The value of the Number is {number}")
 }
